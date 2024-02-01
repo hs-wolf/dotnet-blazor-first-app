00caf88946e4b590dc23e5720982dd7a51d695ac
José Israel <hswolf@proton.me>
2024-02-01 12:26:31 -0300

    Added the improve-navigation project.

---
+++ improve-navigation/Model/PizzaTopping.cs
@@ -0,0 +1,10 @@
+﻿namespace improve_navigation;
+
+public class PizzaTopping
+{
+    public Topping Topping { get; set; }
+
+    public int ToppingId { get; set; }
+
+    public int PizzaId { get; set; }
+}
---
+++ improve-navigation/Model/UserInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace improve_navigation;
+
+public class UserInfo
+{
+    public bool IsAuthenticated { get; set; }
+
+    public string Name { get; set; }
+}
